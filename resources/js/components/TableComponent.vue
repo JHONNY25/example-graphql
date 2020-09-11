c16cc3b5bdbae59dcd7ba240a76f71fd4c0152be
JHONNY25 <37971335+JHONNY25@users.noreply.github.com>
2020-09-11 15:14:13 -0600

    config for graphql and add components

---
+++ resources/js/components/TableComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th v-for="header in headers" :key="header" >{{ header.text }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <slot name="table-content"></slot>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import { argsToArgsConfig } from 'graphql/type/definition';
+
+    export default {
+        props:{
+            headers: []
+        },
+    }
+</script>
